3dfdc0a2e305da8233114b69b86f4c57a06dc3fb
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-06-02 23:19:55 -0400

    Update App.js

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('hello');
+	};
+
+	return (
+		<section className='section-center'>
+			<h3>some text</h3>
+			<form className='lorem-form' onSubmit={handleSubmit}>
+				<label htmlFor='amount'>paragraphs:</label>
+			</form>
+		</section>
+	);
 }
 
 export default App;
