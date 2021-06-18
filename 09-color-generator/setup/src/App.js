770d38e33698693cfa410f2fbea71649582c2bf0
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-06-18 11:19:02 -0400

    .

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -12,6 +12,7 @@ function App() {
 		e.preventDefault();
 		try {
 			let colors = new Values(color).all(10);
+			setList(colors);
 		} catch (error) {
 			setError(true);
 			console.log(error);
@@ -37,7 +38,9 @@ function App() {
 				</form>
 			</section>
 			<section className='colors'>
-				<h4>List goes here</h4>
+				{list.map((color, index) => {
+					return <SingleColor key={index} {...color} index={index} />;
+				})}
 			</section>
 		</>
 	);
